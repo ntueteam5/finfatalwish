4ca3aac71ab279798a96c441e65dd6584e475207
110619036 <kinlong94cr@gmail.com>
2019-06-18 17:22:35 +0800

    twotwo

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,16 @@
 let path = require('path');
 
-let htmlPages = ['index','course'];
+let htmlPages = ['index','character','event','item','madeit','main_page','map','messange','rule'];
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let htmlPlugins = htmlPages.map(page =>
     new HtmlWebpackPlugin({
-        filename: `${character}.html`,
-        template: `assets/${character}.html`,
+        filename: `${page}.html`,
+        template: `assets/${page}.html`,
         favicon:'assets/images/favicon.ico'
     }),
-    new HtmlWebpackPlugin({
-        filename: `${event}.html`,
-        template: `assets/${event}.html`,
-        favicon:'assets/images/favicon.ico'
-    }),
-    new HtmlWebpackPlugin({
-        filename: `${item}.html`,
-        template: `assets/${item}.html`,
-        favicon:'assets/images/favicon.ico'
-    }),
-    new HtmlWebpackPlugin({
-        filename: `${madeit}.html`,
-        template: `assets/${madeit}.html`,
-        favicon:'assets/images/favicon.ico'
-    }),
-    new HtmlWebpackPlugin({
-        filename: `${main_page}.html`,
-        template: `assets/${main_page}.html`,
-        favicon:'assets/images/favicon.ico'
-    }),
-    new HtmlWebpackPlugin({
-        filename: `${map}.html`,
-        template: `assets/${map}.html`,
-        favicon:'assets/images/favicon.ico'
-    }),
-    new HtmlWebpackPlugin({
-        filename: `${messange}.html`,
-        template: `assets/${messange}.html`,
-        favicon:'assets/images/favicon.ico'
-    }),
-    new HtmlWebpackPlugin({
-        filename: `${rule}.html`,
-        template: `assets/${rule}.html`,
-        favicon:'assets/images/favicon.ico'
-    })
 );
 
 module.exports = {
