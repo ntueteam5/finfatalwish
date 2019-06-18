28e2af0d818f747a67ae4a41957fc641168b61fe
110619036 <kinlong94cr@gmail.com>
2019-06-18 13:47:33 +0800

    fifteen

--- assets/js/app.js
+++ assets/js/app.js
@@ -166,6 +166,23 @@ import '../css/normalize.css';
 import '../css/normalizetwo.css';
 import '../css/welcome.css';
 
+import "../css/fonts/PT-Sans-Bold.eot";
+import "../css/fonts/PT-Sans-Bold.svg";
+import "../css/fonts/PT-Sans-Bold.ttf";
+import "../css/fonts/PT-Sans-Bold.woff";
+import "../css/fonts/PT-Sans-BoldItalic.eot";
+import "../css/fonts/PT-Sans-BoldItalic.svg";
+import "../css/fonts/PT-Sans-BoldItalic.ttf";
+import "../css/fonts/PT-Sans-BoldItalic.woff";
+import "../css/fonts/PT-Sans-Italic.eot";
+import "../css/fonts/PT-Sans-Italic.svg";
+import "../css/fonts/PT-Sans-Italic.ttf";
+import "../css/fonts/PT-Sans-Italic.woff";
+import "../css/fonts/PT-Sans.eot";
+import "../css/fonts/PT-Sans.svg";
+import "../css/fonts/PT-Sans.ttf";
+import "../css/fonts/PT-Sans.woff";
+
 
 $('#image__one').attr("src", one);
 $('#image__two').attr("src", two);
